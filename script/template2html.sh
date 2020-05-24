6c8457711092b7bdb8accf3e748cd12c2c0aedc4
bkil <b.K.il.h.u+tigbuh@gmail.com>
2020-05-24 16:46:26 +0200

    script/template2html: no-js inlined table

--- script/template2html.sh
+++ script/template2html.sh
@@ -6,13 +6,19 @@ set -e
 main() {
   local IN="${2-../web/index.html.template}"
   local OUT="${2-../dist/index.html}"
-  mkdir -p "`dirname "$OUT"`" || exit 1
+  mkdir -p "`dirname "$OUT"`"
+
+  ./bad-lines.sh
 
   cat "$IN" |
+  escape_translation > "$OUT"
+}
+
+escape_translation() {
   sed -r "
     s~\{% t '([^']*)' %\}~\1~g
     s~\{% t \"([^\"]*)\" %\}~\1~g
-  " > "$OUT"
+  "
 }
 
 main "$@"
