e6496bfb2793c088654466d347d03c757534345a
bkil <b.K.il.h.u+tigbuh@gmail.com>
2020-05-24 17:08:39 +0200

    script/template2html.sh: static HTML table for both noscript and JS data

--- script/template2html.sh
+++ script/template2html.sh
@@ -6,19 +6,38 @@ set -e
 main() {
   local IN="${2-../web/index.html.template}"
   local OUT="${2-../dist/index.html}"
+  local MARKER="{% GLOSARHTMLTABLE %}"
   mkdir -p "`dirname "$OUT"`"
 
   ./bad-lines.sh
 
-  cat "$IN" |
-  escape_translation > "$OUT"
+  {
+    sed -n "/$MARKER/,$ ! p" "$IN" |
+    escape_translation
+
+    cat "../szotar/glosar.txt" |
+    tsv2html_table
+
+    sed -n "1,/$MARKER/ ! p" "$IN" |
+    escape_translation
+  } > "$OUT"
+}
+
+tsv2html_table() {
+  echo "<table>"
+
+  sed -r "
+    s~^(.*)\t(.*)\t(.*)~ <tr> <td>\1</td><td>\2</td><td>\3</td> </tr>~
+  "
+
+  echo "</table>"
 }
 
 escape_translation() {
   sed -r "
     s~\{% t '([^']*)' %\}~\1~g
     s~\{% t \"([^\"]*)\" %\}~\1~g
-  "
+  " "$@"
 }
 
 main "$@"
