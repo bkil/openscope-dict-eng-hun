06a37c948d033ebb967afcadf15485f2787a9b16
bkil <b.K.il.h.u+tigbuh@gmail.com>
2021-12-05 15:27:26 +0100

    script/make-dict.sh: update format, move result to dist/

--- script/make-dict.sh
+++ script/make-dict.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 main() {
-  IN="../szotar/glosar.txt"
+  IN="../szotar/glosar.csv"
   BASE="OpenScope-szotar2"
 
   cat "$IN" |
-  sed -r "s~^(.*)\t(.*)\t(.*)~:\1:\2 (\3)~" |
+  sed -r "s~^([^\t]*)\t([^\t]*)\t([^\t]*)\t(.*)~:\1:\3 (\2 \4)~" |
   dictfmt \
     --utf8 \
     --allchars \
     -s "OpenScope szotar2" \
     -j "$BASE" &&
   dictzip "$BASE.dict"
+
+  mv -t ../dist/ *.dz *.index
 }
 
 main "$@"
